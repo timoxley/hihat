8febf93c08722f3f4a7c7cea0084124bac6124c2
Tim Oxley <secoif@gmail.com>
2015-06-13 08:01:06 +0800

    Don't crash, log build errors in clientside console.
    Proof-of-concept.
    
    Adds --quit flag. Will quit on build error rather than doing
    aforementioned logging.
    
    Related #3.

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+Error.stackTraceLimit = Infinity;
 var args = process.argv.slice(2)
 var assign = require('object-assign')
+var serializerr = require('serializerr')
 var path = require('path')
 var argv = require('minimist')(args, {
   boolean: ['devtool'],
-  default: { devtool: true }
+  default: { devtool: true },
+  boolean: ['quit'],
+  default: { quit: false}
 })
 
 var app = require('app')
@@ -19,11 +23,18 @@ require('crash-reporter').start()
 
 var hihat
 var mainWindow = null
+var lastError = null
+
 app.on('window-all-closed', close)
 
 process.on('uncaughtException', function (err) {
   process.stderr.write((err.stack ? err.stack : err) + '\n')
-  close()
+  if (argv.quit) {
+    close()
+  } else {
+    lastError = err
+    printLastError()
+  }
 })
 
 function close () {
@@ -65,9 +76,13 @@ function start (opt) {
           mainWindow.openDevTools({ detach: true })
         }
       })
+      mainWindow.webContents.on('dom-ready', function () {
+        printLastError()
+      })
 
       mainWindow.show()
       mainWindow.loadUrl(ev.uri)
+      printLastError()
 
       mainWindow.once('closed', function () {
         mainWindow = null
@@ -77,6 +92,21 @@ function start (opt) {
     .on('update', function () {
       if (mainWindow) {
         mainWindow.reload()
+        lastError = null
       }
     })
+}
+
+function printLastError() {
+  if (!mainWindow || !lastError) return
+  var err = serializerr(lastError)
+  mainWindow.webContents.executeJavaScript([
+    "(function() {",
+    "var errObj = " + JSON.stringify(err),
+    "var err = new Error()",
+    "mixin(err, errObj)",
+    "throw err",
+    "function mixin(a, b) { for (var key in b) a[key] = b[key] }",
+    "})()"
+  ].join('\n'))
 }
